0d1eded079cd6750113f22092f675b61987fb2e3
Jesper Christiansen <jesper@creativegenius.dk>
2011-09-22 22:08:10 +0200

    Added find_name to cash_book_proxy

--- lib/economic/proxies/cash_book_proxy.rb
+++ lib/economic/proxies/cash_book_proxy.rb
@@ -8,22 +8,40 @@ module Economic
           'name' => name
         }
       end
-      
-      handles = [response[:cash_book_handle]].flatten.reject(&:blank?)
 
-      # Create partial Debtor entities
-      handles.collect do |handle|
-        cash_book = build
-        cash_book.partial = true
-        cash_book.persisted = true
-        cash_book.handle = handle
-        cash_book.number = handle[:number]
-        cash_book
-      end
+      cash_book = build
+      cash_book.partial = true
+      cash_book.persisted = true
+      cash_book.number = response[:number]
+      cash_book
+
     end
     
     def all
       response = session.request entity_class.soap_action('GetAll')
+
+      handles = [response[:cash_book_handle]].flatten.reject(&:blank?)
+      cash_books = []
+      handles.each do |handle|
+        cash_books << find_name(handle[:number])
+      end
+      
+      cash_books
+    end
+    
+    def find_name(id)
+      response = session.request entity_class.soap_action("GetName") do
+        soap.body = {
+          'cashBookHandle' => {
+            'Number' => id
+          }
+        }
+      end
+
+      cash_book = build
+      cash_book.number = id
+      cash_book.name = response
+      cash_book
     end
   end
 end
