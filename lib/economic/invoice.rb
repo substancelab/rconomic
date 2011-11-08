f400f0fefb7f608ff3e3f9081ec25baec139cbd3
Jakob Skjerning <jakob@mentalized.net>
2011-11-08 10:42:04 +0100

    Add wrapper for Invoice_GetPdf

--- lib/economic/invoice.rb
+++ lib/economic/invoice.rb
@@ -3,5 +3,20 @@ require 'economic/entity'
 module Economic
   class Invoice < Entity
     has_properties :number, :net_amount, :vat_amount, :due_date, :debtor_handle, :heading
+
+    # Returns the PDF version of Invoice as a String.
+    #
+    # To get it as a file you can do:
+    #
+    #   File.open("invoice.pdf", 'wb') do |file|
+    #     file << invoice.pdf
+    #   end
+    def pdf
+      response = session.request soap_action(:get_pdf) do
+        soap.body = { "invoiceHandle" => handle.to_hash }
+      end
+
+      Base64.decode64(response)
+    end
   end
 end
---
+++ spec/economic/invoice_spec.rb
@@ -0,0 +1,36 @@
+require './spec/spec_helper'
+
+describe Economic::CurrentInvoice do
+  let(:session) { make_session }
+  subject { (i = Economic::Invoice.new( :number => 512 )).tap { i.session = session } }
+
+  it "inherits from Economic::Entity" do
+    Economic::Invoice.ancestors.should include(Economic::Entity)
+  end
+
+  describe ".proxy" do
+    it "should return a InvoiceProxy" do
+      subject.proxy.should be_instance_of(Economic::InvoiceProxy)
+    end
+
+    it "should return a proxy owned by session" do
+      subject.proxy.session.should == session
+    end
+  end
+
+  describe "#pdf" do
+    before :each do
+      savon.stubs('Invoice_GetPdf').returns(:success)
+    end
+
+    it "gets PDF data from API" do
+      savon.expects('Invoice_GetPdf').with('invoiceHandle' => {'Number' => 512}).returns(:success)
+      subject.pdf
+    end
+
+    it "decodes the base64Binary encoded data" do
+      subject.pdf.should == 'This is not really PDF data'
+    end
+  end
+
+end
