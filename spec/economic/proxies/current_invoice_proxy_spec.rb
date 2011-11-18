9a4ff4ca676b20be9e008ee5d84a88724cc5c1f7
Jakob Skjerning <jakob@mentalized.net>
2011-11-18 14:24:07 +0100

    Ensure order of SOAP body arguments in FindByDateInterval

--- spec/economic/proxies/current_invoice_proxy_spec.rb
+++ spec/economic/proxies/current_invoice_proxy_spec.rb
@@ -68,7 +68,7 @@ describe Economic::CurrentInvoiceProxy do
     it "should be able to return a single current invoice" do
       from = Time.now - 60
       unto = Time.now
-      savon.expects('CurrentInvoice_FindByDateInterval').with('first' => from.iso8601, 'last' => unto.iso8601).returns(:single)
+      savon.expects('CurrentInvoice_FindByDateInterval').with('first' => from.iso8601, 'last' => unto.iso8601, :order! => ['first', 'last']).returns(:single)
       savon.expects('CurrentInvoice_GetData').returns(:success)
       results = subject.find_by_date_interval(from, unto)
       results.size.should == 1
@@ -78,7 +78,7 @@ describe Economic::CurrentInvoiceProxy do
     it "should be able to return multiple invoices" do
       from = Time.now - 60
       unto = Time.now
-      savon.expects('CurrentInvoice_FindByDateInterval').with('first' => from.iso8601, 'last' => unto.iso8601).returns(:many)
+      savon.expects('CurrentInvoice_FindByDateInterval').with('first' => from.iso8601, 'last' => unto.iso8601, :order! => ['first', 'last']).returns(:many)
       savon.expects('CurrentInvoice_GetData').returns(:success)
       savon.expects('CurrentInvoice_GetData').returns(:success)
       results = subject.find_by_date_interval(from, unto)
@@ -89,7 +89,7 @@ describe Economic::CurrentInvoiceProxy do
     it "should be able to return nothing" do
       from = Time.now - 60
       unto = Time.now
-      savon.expects('CurrentInvoice_FindByDateInterval').with('first' => from.iso8601, 'last' => unto.iso8601).returns(:none)
+      savon.expects('CurrentInvoice_FindByDateInterval').with('first' => from.iso8601, 'last' => unto.iso8601, :order! => ['first', 'last']).returns(:none)
       results = subject.find_by_date_interval(from, unto)
       results.size.should == 0
     end
