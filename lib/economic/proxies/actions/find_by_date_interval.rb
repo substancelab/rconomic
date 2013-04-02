77e47eb78fc5469ca8a91c71cf5f8f9fada2ecbb
Jakob Skjerning <jakob@mentalized.net>
2013-04-02 17:26:40 +0200

    FindByDateInterval should live inside the Economic namespace

--- lib/economic/proxies/actions/find_by_date_interval.rb
+++ lib/economic/proxies/actions/find_by_date_interval.rb
@@ -1,25 +1,27 @@
-module FindByDateInterval
+module Economic
+  module FindByDateInterval
 
-  # Returns entity objects for a given interval of days.
-  def find_by_date_interval(from, unto)
-    response = session.request entity_class.soap_action("FindByDateInterval") do
-      soap.body = {
-        'first' => from.iso8601,
-        'last' => unto.iso8601,
-        :order! => ['first', 'last']
-      }
+    # Returns entity objects for a given interval of days.
+    def find_by_date_interval(from, unto)
+      response = session.request entity_class.soap_action("FindByDateInterval") do
+        soap.body = {
+          'first' => from.iso8601,
+          'last' => unto.iso8601,
+          :order! => ['first', 'last']
+        }
+      end
+
+      handle_key = "#{Support::String.underscore(entity_class_name)}_handle".intern
+      handles = [ response[handle_key] ].flatten.reject(&:blank?).collect do |handle|
+        Entity::Handle.new(handle)
+      end
+
+      get_data_array(handles).collect do |entity_hash|
+        entity = build(entity_hash)
+        entity.persisted = true
+        entity
+      end
     end
 
-    handle_key = "#{Economic::Support::String.underscore(entity_class_name)}_handle".intern
-    handles = [ response[handle_key] ].flatten.reject(&:blank?).collect do |handle|
-      Economic::Entity::Handle.new(handle)
-    end
-
-    get_data_array(handles).collect do |entity_hash|
-      entity = build(entity_hash)
-      entity.persisted = true
-      entity
-    end
   end
-
 end
