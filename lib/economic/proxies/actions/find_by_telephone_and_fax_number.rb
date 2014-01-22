3d6fef3ccf337e097c9c6df668bca6e38058b769
anderslemke <mail@anderslemke.dk>
2014-01-22 16:05:08 +0100

    Find debtor by phone number

---
+++ lib/economic/proxies/actions/find_by_telephone_and_fax_number.rb
@@ -0,0 +1,18 @@
+module FindByTelephoneAndFaxNumber
+  # Returns handle for debtor with phone or fax number.
+  def find_by_telephone_and_fax_number(number)
+    response = request('FindByTelephoneAndFaxNumber', {
+      'telephoneAndFaxNumber' => number
+    })
+    if response == {}
+      nil
+    else
+      entity = build
+      entity.partial = true
+      entity.persisted = true
+      entity.handle = response[:debtor_handle]
+      entity.number = response[:debtor_handle][:number].to_i
+      entity
+    end
+  end
+end
