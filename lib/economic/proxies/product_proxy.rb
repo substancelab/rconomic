27ccce120cacbcc2365cae8be489cb2748d5b54c
Simon Træls Ravn <cs2@cs2.dk>
2014-05-26 06:17:36 +0200

    Basic implementation of products

---
+++ lib/economic/proxies/product_proxy.rb
@@ -0,0 +1,11 @@
+require 'economic/proxies/entity_proxy'
+require 'economic/proxies/actions/find_by_ci_number'
+require 'economic/proxies/actions/find_by_handle_with_number'
+require 'economic/proxies/actions/find_by_number'
+
+module Economic
+  class ProductProxy < EntityProxy
+    include FindByHandleWithNumber
+    include FindByNumber
+  end
+end
