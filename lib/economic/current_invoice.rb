6d6a5ce71ae7417480af1b7555d5314f3ace2fe8
Jakob Skjerning <jakob@mentalized.net>
2016-03-06 21:08:21 +0100

    Use new style property definitions for CurrentInvoice

--- lib/economic/current_invoice.rb
+++ lib/economic/current_invoice.rb
@@ -26,41 +26,30 @@ module Economic
   #
   #   invoice.save
   class CurrentInvoice < Entity
-    has_properties :id,
-      :debtor_handle,
-      :debtor_name,
-      :debtor_address,
-      :debtor_postal_code,
-      :debtor_city,
-      :debtor_country,
-      :attention_handle,
-      :date,
-      :term_of_payment_handle,
-      :due_date,
-      :currency_handle,
-      :exchange_rate,
-      :is_vat_included,
-      :layout_handle,
-      :delivery_date,
-      :net_amount,
-      :vat_amount,
-      :gross_amount,
-      :margin,
-      :margin_as_percent,
-      :heading,
-      :text_line1,
-      :text_line2
-
-    defaults(
-      :id => 0,
-      :date => Time.now,
-      :exchange_rate => 100,
-      :net_amount => 0,
-      :vat_amount => 0,
-      :gross_amount => 0,
-      :margin => 0,
-      :margin_as_percent => 0
-    )
+    property(:id, :required => true, :default => 0)
+    property(:debtor_handle, :formatter => Properties::Formatters::HANDLE)
+    property(:debtor_name, :required => true)
+    property(:debtor_address)
+    property(:debtor_postal_code)
+    property(:debtor_city)
+    property(:debtor_country)
+    property(:attention_handle, :formatter => Properties::Formatters::HANDLE)
+    property(:date, :formatter => Properties::Formatters::DATE, :default => Time.now)
+    property(:term_of_payment_handle, :formatter => Properties::Formatters::HANDLE)
+    property(:due_date, :formatter => Properties::Formatters::DATE, :required => true)
+    property(:currency_handle, :formatter => Properties::Formatters::HANDLE)
+    property(:exchange_rate, :default => 100)
+    property(:is_vat_included, :required => true)
+    property(:layout_handle, :formatter => Properties::Formatters::HANDLE)
+    property(:delivery_date, :formatter => Properties::Formatters::DATE, :required => true)
+    property(:heading)
+    property(:text_line1)
+    property(:text_line2)
+    property(:net_amount, :required => true, :default => 0)
+    property(:vat_amount, :required => true, :default => 0)
+    property(:gross_amount, :required => true, :default => 0)
+    property(:margin, :required => true, :default => 0)
+    property(:margin_as_percent, :required => true, :default => 0)
 
     def initialize(properties = {})
       super
@@ -141,41 +130,6 @@ module Economic
         invoice_line.invoice = self
         invoice_line.save
       end
-    end
-
-    protected
-
-    def fields
-      date_formatter = proc { |date|
-        date.respond_to?(:iso8601) ? date.iso8601 : nil
-      }
-      to_hash = proc { |handle| handle.to_hash }
-      [
-        ["Id", :id, nil, :required],
-        ["DebtorHandle", :debtor, proc { |d| d.handle.to_hash }],
-        ["DebtorName", :debtor_name, nil, :required],
-        ["DebtorAddress", :debtor_address],
-        ["DebtorPostalCode", :debtor_postal_code],
-        ["DebtorCity", :debtor_city],
-        ["DebtorCountry", :debtor_country],
-        ["AttentionHandle", :attention_handle, to_hash],
-        ["Date", :date, date_formatter],
-        ["TermOfPaymentHandle", :term_of_payment_handle, to_hash],
-        ["DueDate", :due_date, date_formatter, :required],
-        ["CurrencyHandle", :currency_handle, to_hash],
-        ["ExchangeRate", :exchange_rate],
-        ["IsVatIncluded", :is_vat_included, nil, :required],
-        ["LayoutHandle", :layout_handle, to_hash],
-        ["DeliveryDate", :delivery_date, date_formatter, :required],
-        ["Heading", :heading],
-        ["TextLine1", :text_line1],
-        ["TextLine2", :text_line2],
-        ["NetAmount", :net_amount, nil, :required],
-        ["VatAmount", :vat_amount, nil, :required],
-        ["GrossAmount", :gross_amount, nil, :required],
-        ["Margin", :margin, nil, :required],
-        ["MarginAsPercent", :margin_as_percent, nil, :required]
-      ]
     end
   end
 end
--- lib/economic/entity/properties/formatters.rb
+++ lib/economic/entity/properties/formatters.rb
@@ -3,6 +3,7 @@ module Economic
     module Properties
       # Utilities for formatting various data types to SOAP fields
       module Formatters
+        DATE = proc { |date| date.respond_to?(:iso8601) ? date.iso8601 : nil }
         HANDLE = proc { |handle| handle.to_hash }
       end
     end
