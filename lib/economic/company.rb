2f71d5c5992722e5406a7d9e62da1f4edbd8af6e
Jakob Skjerning <jakob@mentalized.net>
2016-03-06 13:05:51 +0100

    Use new style property definitions for Company

--- lib/economic/company.rb
+++ lib/economic/company.rb
@@ -11,51 +11,27 @@ module Economic
   #   creditor = economic.company.get
   #
   class Company < Entity
-    has_properties :number,
-      :base_currency_handle,
-      :name,
-      :address_1,
-      :address_2,
-      :postal_code,
-      :city,
-      :country,
-      :telephone_number,
-      :fax_number,
-      :mobile_number,
-      :contact,
-      :website,
-      :email,
-      :c_i_number,
-      :vat_number,
-      :sign_up_date
+    property(:handle, :formatter => proc { |h| h.to_hash }, :required => true)
+    property(:number, :required => true)
+    property(:base_currency_handle, :formatter => proc { |h| h.to_hash })
+    property(:name)
+    property(:address_1)
+    property(:address_2)
+    property(:postal_code)
+    property(:city)
+    property(:country)
+    property(:telephone_number)
+    property(:fax_number)
+    property(:mobile_number)
+    property(:contact)
+    property(:website)
+    property(:email)
+    property(:c_i_number)
+    property(:vat_number)
+    property(:sign_up_date)
+
     def handle
       @handle || Handle.build(:number => @number)
-    end
-
-    protected
-
-    def fields
-      to_hash = proc { |h| h.to_hash }
-      [
-        ["Handle", :handle, to_hash, :required],
-        ["Number", :number, nil, :required],
-        ["BaseCurrencyHandle", :base_currency_handle, to_hash],
-        ["Name", :name],
-        ["Address1", :address_1],
-        ["Address2", :address_2],
-        ["PostalCode", :postal_code],
-        ["City", :city],
-        ["Country", :country],
-        ["TelephoneNumber", :telephone_number],
-        ["FaxNumber", :fax_number],
-        ["MobileNumber", :mobile_number],
-        ["Contact", :contact],
-        ["WebSite", :website],
-        ["Email", :email],
-        ["CINumber", :c_i_number],
-        ["VatNumber", :vat_number],
-        ["SignUpDate", :sign_up_date]
-      ]
     end
   end
 end
