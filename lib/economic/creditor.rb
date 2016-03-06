ac768ddc79ec219ee348d9761a5f79fa030c64aa
Jakob Skjerning <jakob@mentalized.net>
2016-03-06 14:39:38 +0100

    Use new style property definitions for Creditor

--- lib/economic/creditor.rb
+++ lib/economic/creditor.rb
@@ -21,7 +21,28 @@ module Economic
   #   creditor.term_of_payment = 1
   #   creditor.save
   class Creditor < Entity
-    has_properties :number, :creditor_group_handle, :name, :vat_zone, :currency_handle, :term_of_payment_handle, :is_accessible, :ci_number, :email, :address, :postal_code, :city, :country, :bank_account, :attention_handle, :your_reference_handle, :our_reference_handle, :default_payment_type_handle, :default_payment_creditor_id, :county, :auto_contra_account_handle
+    property(:handle, :formatter => proc { |h| h.to_hash }, :required => true)
+    property(:number, :required => true)
+    property(:creditor_group_handle, :formatter => proc { |h| h.to_hash })
+    property(:name)
+    property(:vat_zone)
+    property(:currency_handle, :formatter => proc { |h| h.to_hash })
+    property(:term_of_payment_handle, :formatter => proc { |h| h.to_hash })
+    property(:is_accessible)
+    property(:ci_number)
+    property(:email)
+    property(:address)
+    property(:postal_code)
+    property(:city)
+    property(:country)
+    property(:bank_account)
+    property(:attention_handle, :formatter => proc { |h| h.to_hash })
+    property(:your_reference_handle, :formatter => proc { |h| h.to_hash })
+    property(:our_reference_handle, :formatter => proc { |h| h.to_hash })
+    property(:default_payment_type_handle, :formatter => proc { |h| h.to_hash })
+    property(:default_payment_creditor_id, :formatter => proc { |h| h.to_hash })
+    property(:county)
+    property(:auto_contra_account_handle, :formatter => proc { |h| h.to_hash })
 
     def handle
       @handle || Handle.build(:number => @number)
@@ -30,36 +51,6 @@ module Economic
     # Returns the Creditors contacts
     def contacts
       @contacts ||= CreditorContactProxy.new(self)
-    end
-
-    protected
-
-    def fields
-      to_hash = proc { |h| h.to_hash }
-      [
-        ["Handle", :handle, to_hash, :required],
-        ["Number", :number, nil, :required],
-        ["CreditorGroupHandle", :creditor_group_handle, to_hash],
-        ["Name", :name],
-        ["VatZone", :vat_zone],
-        ["CurrencyHandle", :currency_handle, to_hash],
-        ["TermOfPaymentHandle", :term_of_payment_handle, to_hash],
-        ["IsAccessible", :is_accessible],
-        ["CINumber", :ci_number],
-        ["Email", :email],
-        ["Address", :address],
-        ["PostalCode", :postal_code],
-        ["City", :city],
-        ["Country", :country],
-        ["BankAccount", :bank_account],
-        ["AttentionHandle", :attention_handle, to_hash],
-        ["YourReferenceHandle", :your_reference_handle, to_hash],
-        ["OurReferenceHandle", :our_reference_handle, to_hash],
-        ["DefaultPaymentTypeHandle", :default_payment_type_handle, to_hash],
-        ["DefaultPaymentCreditorId", :default_payment_creditor_id, to_hash],
-        ["County", :county],
-        ["AutoContraAccountHandle", :auto_contra_account_handle, to_hash]
-      ]
     end
   end
 end
