df11fd7f40e4cd564124bc3a18bf9814b963ef91
Mathias Jakobsen <mathiasjakobsen@me.com>
2016-07-12 12:54:37 +0200

    Adds otherreference to invoice

--- lib/economic/invoice.rb
+++ lib/economic/invoice.rb
@@ -16,7 +16,8 @@ module Economic
       :debtor_country,
       :debtor_ean,
       :attention_handle,
-      :heading
+      :heading,
+      :other_reference
 
     def attention
       return nil if attention_handle.nil?
