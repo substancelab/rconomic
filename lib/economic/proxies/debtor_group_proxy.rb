6ac718a651a75122ebfdec5e0ffdd67b20b78d87
Mathias Jakobsen <mathiasjakobsen@me.com>
2016-07-15 11:03:20 +0200

    debtor group .find shoudl find bu number handle.

--- lib/economic/proxies/debtor_group_proxy.rb
+++ lib/economic/proxies/debtor_group_proxy.rb
@@ -5,6 +5,11 @@ module Economic
   class DebtorGroupProxy < EntityProxy
     include FindByNumber
 
+    def find(handle)
+      handle = Entity::Handle.build(:number => handle) unless handle.is_a?(Entity::Handle)
+      super(handle)
+    end
+
     def find_by_name(name)
       Proxies::Actions::FindByName.new(self, name).call
     end
