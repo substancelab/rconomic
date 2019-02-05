f5532c4e614708959ed52c7d578c575f9e329c3e
Ole Palm <olepalm@hotmail.com>
2019-02-05 12:48:09 +0100

    An orderline can be updated
    As we now have mappings for the OrderLine fields

--- lib/economic/order_line.rb
+++ lib/economic/order_line.rb
@@ -20,5 +20,36 @@ module Economic
       :department_handle,
       :distribution_key_handle,
       :inventory_location_handle
+
+    def handle
+      @handle || Handle.build(:number => @number)
+    end
+
+    protected
+
+    # Returns the field rules to use when mapping to SOAP data
+    def fields
+      to_hash = proc { |handle| handle.to_hash }
+      [
+        ["Handle", :handle, to_hash],
+        ["Id", :number],
+        ["Number", :number],
+        ["OrderHandle", :order_handle, to_hash],
+        ["Description", :description],
+        ["DeliveryDate", :delivery_date, nil, :required],
+        ["UnitHandle", :unit_handle, to_hash],
+        ["ProductHandle", :product_handle, to_hash],
+        ["Quantity", :quantity],
+        ["UnitNetPrice", :unit_net_price, nil, :required],
+        ["DiscountAsPercent", :discount_as_percent],
+        ["UnitCostPrice", :unit_cost_price],
+        ["TotalNetAmount", :total_net_amount, nil, :required],
+        ["TotalMargin", :total_margin],
+        ["MarginAsPercent", :margin_as_percent],
+        ["DepartmentHandle", :department_handle, to_hash],
+        ["DistributionKeyHandle", :distribution_key_handle, to_hash],
+        ["InventoryLocationHandle", :inventory_location_handle, to_hash]
+      ]
+    end
   end
 end
